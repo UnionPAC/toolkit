81ac79cc3f859634cccadc42338858c013d79262
Geoff Jamieson <geoff.ijamieson@gmail.com>
2025-05-17 01:19:00 -0400

    add demo RotatingWords

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
-<script setup>
-</script>
-
 <template>
   <main>
-    <p class="text-4xl">hello world</p>
+    <RotatingWords
+      staticTextBefore="I'm a"
+      :words="['Wizard', 'Climber', 'Designer']"
+      staticTextAfter="and I love Vue."
+      animation="slide-left"
+      interval="2000"
+      wrapperClass="gap-1"
+      wordClass="text-blue-500 font-bold"
+    />
   </main>
 </template>
+
+<script setup>
+import { RotatingWords } from '../lib';
+</script>
 
 <style scoped>
 </style>
