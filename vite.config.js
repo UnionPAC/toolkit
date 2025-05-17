eb41965fe1232c3d18ad49768f4b8bb46759a015
Geoff Jamieson <geoff.ijamieson@gmail.com>
2025-05-17 00:38:32 -0400

    configure vite library mode

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
+import { dirname, resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/index.js'),
+      name: 'MyToolkit',
+      fileName: 'my-toolkit',
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        },
+        exports: 'named'
+      }
+    }
+  },
   plugins: [
     vue(),
     vueDevTools(),
